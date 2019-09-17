8c1211a06a89e86004f4c8a51fb4fa4ba2b4c974
Shraeya Srinivasan <srinivasan.shr@husky.neu.edu>
2019-09-17 14:31:43 -0400

    “comment”

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function multiplyBy(){
-	// Insert your code here
+function multiplyNums()
+{
+        num1 = document.getElementById("numberOne").value;
+        num2 = document.getElementById("numberTwo").value;
+        document.getElementById("result").innerHTML = num1 * num2;
 }
 
-function divideBy(){ 
-	// Insert your code here
+function divideNums() 
+{ 
+        num1 = document.getElementById("numberOne").value;
+		num2 = document.getElementById("numberTwo").value;
+		document.getElementById("result").innerHTML = num1 / num2;
 }
